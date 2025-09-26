80ede8a9d2c65843c41866daefd5bd6d3c07b5cb
Cristivn <cristian4lorca@gmail.com>
2025-09-26 15:18:10 -0300

    feat:actualizacion del models

--- AgendaContactosPersonales/AgendaPersonal/models.py
+++ AgendaContactosPersonales/AgendaPersonal/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Contacto (models.Model):
+    nombre = models.CharField(max_length=100)
+    telefono = models.CharField(max_length=12)
+    correo = models.EmailField()
+    direccion = models.CharField(max_length=100) 
